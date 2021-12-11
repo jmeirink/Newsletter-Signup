2f1111dd8cf78000c5eff6de524973d7389072a8
jmeirink <84925553+jmeirink@users.noreply.github.com>
2021-12-11 11:19:51 -0600

    Made updates to the app.js file.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 
 const app = express();
@@ -33,6 +34,24 @@ app.post("/", function(req, res){
   };
 
   const jsonData = JSON.stringify(data);
+
+
+// Might need to replace <dc> with us20
+  const url = "https://<dc>.api.mailchimp.com/3.0/lists/f238cb0a12"
+
+  const options = {
+    method: "POST",
+    auth: "jmeirink1:d3c15d13cad1054d4426c1aa531e319d-us20"
+  }
+
+  const request = https.request(url, options, function(response){
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
 
 });
 
